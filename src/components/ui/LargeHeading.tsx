2073934fa2760dcf7a3ac03de2b0ef29fdffc7a4
albjoh <aljo940616@gmail.com>
2023-04-24 21:42:53 +0200

    Finnishing up with the navbar components

--- src/components/ui/LargeHeading.tsx
+++ src/components/ui/LargeHeading.tsx
@@ -1,6 +1,6 @@
 import { HTMLAttributes, forwardRef } from "react";
 import { VariantProps, cva } from "class-variance-authority";
-import { cn } from "@/app/lib/utils";
+import { cn } from "@/lib/utils";
 
 const headingVariants = cva(
   "text-black dark:tet-white text-center lg:text-left font-extrabold leading-tight tracking-tighter",
