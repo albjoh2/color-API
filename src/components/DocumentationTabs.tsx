11bbfc0e16d775f5b6381eba9fb06a81c4c67537
albjoh <aljo940616@gmail.com>
2023-04-25 10:56:29 +0200

    documentation page done

---
+++ src/components/DocumentationTabs.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { FC } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/Tabs";
+import Code from "@/components/Code";
+import SimpleBar from "simplebar-react";
+import { nodejs, python } from "@/helpers/documentation-code";
+
+const DocumentetionTabs: FC = ({}) => {
+  return (
+    <Tabs defaultValue="nodejs" className="max-w-2xl w-full">
+      <TabsList>
+        <TabsTrigger value="nodejs">NodeJS</TabsTrigger>
+        <TabsTrigger value="python">Python</TabsTrigger>
+      </TabsList>
+      <TabsContent value="nodejs">
+        <SimpleBar>
+          <Code animated language="javascript" code={nodejs} show />
+        </SimpleBar>
+      </TabsContent>
+      <TabsContent value="python">
+        <SimpleBar>
+          <Code animated language="python" code={python} show />
+        </SimpleBar>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+export default DocumentetionTabs;
