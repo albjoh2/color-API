36ca55e14967c91ac42ac0c69f765abfa6d6fa7f
albjoh <aljo940616@gmail.com>
2023-04-26 11:50:16 +0200

    Dashboardpage working

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ButtonHTMLAttributes, FC } from "react";
+import { Copy } from "lucide-react";
+import Button from "./ui/Button";
+import { toast } from "./ui/Toast";
+
+interface CopyButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  valueToCopy: string;
+}
+
+const CopyButton: FC<CopyButtonProps> = ({
+  valueToCopy,
+  className,
+  ...props
+}) => {
+  return (
+    <Button
+      {...props}
+      onClick={() => {
+        navigator.clipboard.writeText(valueToCopy);
+
+        toast({
+          title: "Copied",
+          message: "Copied to clipboard",
+          type: "success",
+        });
+      }}
+      variant="ghost"
+      className={className}
+    >
+      <Copy className="h-5 w-5" />
+    </Button>
+  );
+};
+
+export default CopyButton;
